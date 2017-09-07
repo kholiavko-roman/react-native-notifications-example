ee549662969b37757a1f33ade7676edba4ebe418
Роман Холявко <wildklas@gmail.com>
2017-09-08 02:27:57 +0300

    Initial commit

---
+++ .history/index.android_20170908000037.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import {NotificationsAndroid} from 'react-native-notifications';
+
+
+
+export default class testpush extends Component {
+  componentWillMount() {
+      NotificationsAndroid.refreshToken();
+
+      NotificationsAndroid.setRegistrationTokenUpdateListener(onPushRegistered);
+
+      function onPushRegistered(token) {
+        console.log('token is');
+        console.log(token);
+      }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('testpush', () => testpush);
